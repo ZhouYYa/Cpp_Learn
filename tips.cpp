f4e9020c27f40c9e1d69b7611c5ad84b729bcf3b
ZhouYYa <39910463+ZhouYYa@users.noreply.github.com>
2024-06-28 09:55:37 +0800

    Update tips.cpp

--- tips.cpp
+++ tips.cpp
@@ -1,3 +1,8 @@
 
 std::shared_ptr<Type> m_pType;
 m_pType = std::make_shared<Type>();
+
+std::unique_ptr<Type> m_pType;
+m_pType = std::make_unique<Type>();
+
+Type* GetType() { return m_pType.get(); }
