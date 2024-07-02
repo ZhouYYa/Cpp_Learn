325df7ee087feebaecbddc5ee25b10f1762844a6
ZhouYYa <39910463+ZhouYYa@users.noreply.github.com>
2024-07-02 17:21:21 +0800

    Update tips.cpp

--- tips.cpp
+++ tips.cpp
@@ -1,4 +1,4 @@
-
+//////////// 对指针的使用 ////////////////
 std::shared_ptr<Type> m_pType;
 m_pType = std::make_shared<Type>();
 
@@ -6,3 +6,10 @@ std::unique_ptr<Type> m_pType;
 m_pType = std::make_unique<Type>();
 
 Type* GetType() { return m_pType.get(); }
+//////////// 对指针的使用 ////////////////
+
+//////////// 对字符串的操作 ////////////////
+boost::algorithm::replace_all(latestName, "/", "\\");
+boost::algorithm::replace_all(latestName, "\\\\", "\\");
+curproject = latestName.substr(0, latestName.rfind('\\'));
+curproject = curproject.substr(curproject.rfind('\\') + 1);
