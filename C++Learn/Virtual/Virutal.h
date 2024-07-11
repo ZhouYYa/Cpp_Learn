e1957fa9c39df2b2146d5a637b845baae9ea80d1
ZhouYYa <985736711@qq.com>
2024-07-11 23:25:58 +0800

    add virtual Delegation+Inherition

--- C++Learn/Virtual/Virutal.h
+++ C++Learn/Virtual/Virutal.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <iostream>
+
 class Virutal
 {
 public:
@@ -14,3 +17,148 @@ public:
 
 
 // Template Method
+
+
+// Inheritance + Compositon
+namespace IC {
+    // 构造顺序： Base -> Componet -> Derived
+    class Base {
+    public:
+        Base() {
+            std::cout << "this is Base ctor." << std::endl;
+        }
+        ~Base() {
+            std::cout << "this is Base dtor." << std::endl;
+        }
+    };
+
+    class Component {
+    public:
+        Component() {
+            std::cout << "this is Component ctor." << std::endl;
+        }
+        ~Component() {
+            std::cout << "this is Component dtor." << std::endl;
+        }
+        void func() {
+            std::cout << "component fun." << std::endl;
+        }
+    };
+
+    class Derived : public Base {
+        friend class Component;
+    public:
+        Derived() {
+            std::cout << "this is Derived ctor." << std::endl;
+        }
+        ~Derived() {
+            std::cout << "this is Derived dtor." << std::endl;
+        }
+    public:
+        Component com;
+        void func() {
+            std::cout << "Derived func." << std::endl;
+            com.func();
+        }
+    };
+}
+
+namespace IC2 {
+    // 构造顺序： Componet -> Base -> Derived
+    class Component {
+    public:
+        Component() {
+            std::cout << "this is Component ctor." << std::endl;
+        }
+        ~Component() {
+            std::cout << "this is Component dtor." << std::endl;
+        }
+        void func() {
+            std::cout << "component fun." << std::endl;
+        }
+    };
+    class Base {
+        friend class Component;
+    public:
+        Base() {
+            std::cout << "this is Base ctor." << std::endl;
+        }
+        ~Base() {
+            std::cout << "this is Base dtor." << std::endl;
+        }
+        Component com;
+        void func() {
+            std::cout << "Base func." << std::endl;
+            com.func();
+        }
+        
+
+    };
+    class Derived : public Base {
+        //friend class Component;
+    public:
+        Derived() {
+            std::cout << "this is Derived ctor." << std::endl;
+        }
+        ~Derived() {
+            std::cout << "this is Derived dtor." << std::endl;
+        }
+    public:
+        //Component com;
+        void func() {
+            std::cout << "Derived func." << std::endl;
+            //com.func();
+        }
+    };
+}
+
+
+// Delegation + Inheritance
+namespace DI {
+    class Observer {
+    public:
+        virtual void update() {};
+    };
+    class Subject {
+        int m_value;
+        vector<Observer*> m_views;
+    };
+
+    // Composite
+    class Component 
+    {
+        int value;
+    public:
+        Component(int val) { value = val; }
+        virtual void add(Component*) { }
+    };
+
+    class Primitive :public Component
+    {
+    public:
+        Primitive(int val) :Component(val) { }
+    };
+
+    class Composite :public Component
+    {
+        vector<Component*> c;
+    public:
+        Composite(int val) :Component(val) { }
+        void add(Composite* e) {
+            c.push_back(e);
+        }
+    };
+
+    // Prototype
+
+
+}
+int main()
+{
+    IC::Derived a;
+    a.func();
+
+    IC2::Derived b;
+    b.func();
+    return 0;
+}
