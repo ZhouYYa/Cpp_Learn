bdfdc81befb5542f86125b0543ad10c9f90ca142
ZhouYYa <39910463+ZhouYYa@users.noreply.github.com>
2024-07-16 10:32:19 +0800

    Update tips.cpp

--- tips.cpp
+++ tips.cpp
@@ -14,6 +14,30 @@ boost::algorithm::replace_all(latestName, "\\\\", "\\");
 curproject = latestName.substr(0, latestName.rfind('\\'));
 curproject = curproject.substr(curproject.rfind('\\') + 1);
 
+/////////// 获取日期 ////////////////////
+SYSTEMTIME systemTime;
+GetLocalTime(&systemTime);
+std::stringstream ss;
+ss << std::setw(4) << std::setfill('0') << systemTime.wYear << "-"
+    << std::setw(2) << std::setfill('0') << systemTime.wMonth << "-"
+    << std::setw(2) << std::setfill('0') << systemTime.wDay;
+std::string curDate = ss.str();
+
+///////////获取当前程序路径////////////////
+    TCHAR rootPath[MAX_PATH];
+    GetModuleFileName(NULL, rootPath, MAX_PATH);
+    std::string uiRootPath;
+#ifdef _UNICODE
+    std::wstring ws(rootPath);
+    uiRootPath = std::string(ws.begin(), ws.end());
+#else
+    uiRootPath = std::string(tcharString);
+#endif
+    uiRootPath = uiRootPath.substr(0, uiRootPath.rfind('\\'));
+    uiRootPath += "/uilogs/";
+    QString localUiLog = QString::fromStdString(uiRootPath);
+    QString serverUiLog = "/UiLog";
+
 //////////// union ////////////////
 typedef union
     {
@@ -31,6 +55,7 @@ typedef union
         uint32_t m_value;
     }EVT_TRIGGER_REG_TRIGGER_WORD;
 
+/////////////////class template//////////////////
 template<typename T>
 auto GetRaw(T& configPtr, const QString& fileName) -> typename T::pointer
 {
